b9eba4563930d597af41a40a20a8a1ed13a89fe0
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-21 17:37:15 +0530

    database connect, all crucial data in dotenv file and controller set up done

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const signup = (req, res) => {
+  res.send("signup");
+};
+
+const login = (req, res) => {
+  res.send("login");
+};
+
+const logout = (req, res) => {
+  res.send("logout");
+};
+
+export { signup, login, logout };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log(`server is running on port 5000`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
