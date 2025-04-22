e693e6b217e47954e6ded69c185ebc2e02fe2c96
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-22 12:18:55 +0530

    signup controller logic done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,48 @@
-const signup = (req, res) => {
-  res.send("signup");
+import generateToken from "../utils/generateWebToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long." });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "email already exist" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      const createdNewUser = await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (err) {
+    console.log("Error in signup controller", err.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 const login = (req, res) => {
