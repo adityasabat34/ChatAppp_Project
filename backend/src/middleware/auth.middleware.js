f57fc0cf122c7bf42d60a86bead32e32c700210b
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-23 17:33:04 +0530

    auth middleware set up for token verification

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+
+const protect = asyncHandler(async (req, res) => {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized - No token found");
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!decoded) {
+    throw new ApiError(401, "Unauthorized - Invalid token");
+  }
+
+  const user = await User.findById(decoded.userId).select("-password");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  req.user = user;
+  next();
+});
+
+export { protect };
