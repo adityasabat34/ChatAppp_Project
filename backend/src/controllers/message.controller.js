387e67bf00f2353a119c461d5953a4e3e766cd37
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-24 14:02:10 +0530

    All user data controller done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,17 @@
+import User from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const getAllUsers = asyncHandler(async (req, res) => {});
+const getAllUsers = asyncHandler(async (req, res) => {
+  const loggedInUserId = req.user?._id;
+
+  const filteredUsers = await User.find({ id: { $ne: loggedInUserId } }).select(
+    "-password"
+  );
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, filteredUsers, "All User data fetched"));
+});
 
 export { getAllUsers };
