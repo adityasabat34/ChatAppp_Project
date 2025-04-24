fb6d4bcad844bdf35dc809a5d89dfa7d683fd9ed
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-24 17:34:17 +0530

    get previous and latest message chat endpoint done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -5,13 +6,36 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const getAllUsers = asyncHandler(async (req, res) => {
   const loggedInUserId = req.user?._id;
 
-  const filteredUsers = await User.find({ id: { $ne: loggedInUserId } }).select(
-    "-password"
-  );
+  const filteredUsers = await User.find({
+    _id: { $ne: loggedInUserId },
+  }).select("-password");
 
   res
     .status(200)
     .json(new ApiResponse(200, filteredUsers, "All User data fetched"));
 });
 
-export { getAllUsers };
+const getMesaages = asyncHandler(async (req, res) => {
+  const { id: userToChatId } = req.params;
+
+  const myId = req.user._id;
+
+  const messages = await Message.find({
+    $or: [
+      { senderId: myId, recieverId: userToChatId },
+      { senderId: userToChatId, recieverId: myId },
+    ],
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        messages,
+        "previous and latest messages fetched successfully"
+      )
+    );
+});
+
+export { getAllUsers, getMesaages };
