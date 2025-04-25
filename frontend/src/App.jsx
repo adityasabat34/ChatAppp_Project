20427e419963b6eb5ef320ebe05ae12a6fb35bf5
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-25 13:38:30 +0530

    pages and components folder cretaed and routing initiated in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignUpPage";
+import SettingPage from "./pages/SettingPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
-  return <div className="text-red-900 text-center text-2xl">App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
