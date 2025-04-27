4e21d541d40b9b1fd3e9082b19b609df650dd588
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-27 20:21:51 +0530

    sign up page is on build-up process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,52 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import SignUpPage from "./pages/SignUpPage";
 import SettingPage from "./pages/SettingPage";
 import LoginPage from "./pages/LoginPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="animate-spin size-12" />
+      </div>
+    );
+  }
+
+  console.log(authUser);
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
         <Route path="/setting" element={<SettingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { isSigningUp, chedkAuth } = useAuthStore();
+
+  const validateForm = () => {};
+
+  const handleSubmit = () => {};
+
+  return;
 };
 
 export default SignUpPage;
