d90706c0ae1025d13ae978eed495d575705db9b3
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-28 18:03:36 +0530

    icons bug fix and implemented react-icon into it

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Eye, EyeOff, Mail, MessageSquare, User, Loader2 } from "lucide-react";
+import { Mail, Loader2 } from "lucide-react";
 import { Link } from "react-router-dom";
+import { FaUserCircle, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { LuMessageCircleMore } from "react-icons/lu";
+import { TbLockPassword } from "react-icons/tb";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -30,7 +33,7 @@ const SignUpPage = () => {
                 className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
               group-hover:bg-primary/20 transition-colors"
               >
-                <MessageSquare className="size-6 text-primary" />
+                <LuMessageCircleMore size={100} />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">
@@ -46,12 +49,13 @@ const SignUpPage = () => {
                 <span className="label-text font-medium">Full Name</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="size-5 text-base-content/40" />
+                {/* ICON */}
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+                  <FaUserCircle className="text-gray-400 text-xl" size={20} />
                 </div>
                 <input
                   type="text"
-                  className={`input input-bordered w-full pl-10`}
+                  className="input input-bordered w-full pl-10"
                   placeholder="John Doe"
                   value={formData.fullName}
                   onChange={(e) =>
@@ -66,7 +70,7 @@ const SignUpPage = () => {
                 <span className="label-text font-medium">Email</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
                   <Mail className="size-5 text-base-content/40" />
                 </div>
                 <input
@@ -86,8 +90,8 @@ const SignUpPage = () => {
                 <span className="label-text font-medium">Password</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <User className="size-5 text-base-content/40" />
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
+                  <TbLockPassword size={20} />
                 </div>
                 <input
                   type="text"
@@ -104,9 +108,9 @@ const SignUpPage = () => {
                   onClick={() => setShowPassword(!showPassword)}
                 >
                   {showPassword ? (
-                    <EyeOff className="size-5 text-base-content/40" />
+                    <FaRegEyeSlash size={20} />
                   ) : (
-                    <Eye className="size-5 text-base-content/40" />
+                    <FaRegEye size={20} />
                   )}
                 </button>
               </div>
