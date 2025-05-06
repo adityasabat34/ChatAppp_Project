6d5956436283c3bdc595025ce30090ce44da817d
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-06 11:53:25 +0530

    working on UI theme and ui updated in some pages

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -28,7 +28,7 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="h-screen pt-10">
+    <div className="min-h-screen pt-10">
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-lg p-6 space-y-8">
           <div className="text-center">
