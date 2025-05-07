c5b5d9e2aca7b934981488c2abdd24f33c8d43a5
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-07 13:04:13 +0530

    get users from chatMessages section logic implement

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import axiosInstance from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectUser: null,
+  isUserLoading: false,
+  isMessageLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/message/users");
+      set({ users: res.data.data });
+      toast.success("Users fetched successfully");
+    } catch (err) {
+      console.error("Error in getUsers:", err?.response?.data || err.message);
+      const errorMessage =
+        err?.response?.data?.message || err?.response?.data || err.message;
+      toast.error(errorMessage);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+}));
