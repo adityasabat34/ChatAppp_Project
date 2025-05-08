8731a0dfd83525d83b54680ff2d03bc0d896d4da
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-08 21:49:37 +0530

    side bar Loading component built

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import SidebarSkeleton from "./skeletons/SideBarSkeleton";
 
 const Sidebar = () => {
+  const { users, getUsers, getSelectedUser, isUserLoading, selectedUser } =
+    useChatStore();
+
+  const onlineUsers = [];
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  if (isUserLoading) return <SidebarSkeleton />;
   return <div>Sidebar</div>;
 };
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import NoChatContainer from "../components/NoChatContainer";
+import ChatContainer from "../components/ChatContainer";
+import { useChatStore } from "../store/useChatStore";
+import { Sidebar } from "lucide-react";
 
 const HomePage = () => {
+  const { selectedUser } = useChatStore();
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <NoChatContainer />
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-20">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {!selectedUser ? <NoChatContainer /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
