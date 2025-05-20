36702f92145d6c36cc4557adfd771abf83d8cf5c
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-20 11:20:16 +0530

    bug fixed, issue with express v5 and solved and READY FOR PRODUCTION

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
+import express from "express";
+import path from "path";
+import { app, server } from "./lib/socket.js";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
-import { app, server } from "./lib/socket.js";
-import express from "express";
-import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -28,7 +28,7 @@ app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
